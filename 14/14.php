4d137fca16b69776db62b206541913959fc23bb9
ahmedkoresh500 <ahmedkoresh500@gmail.com>
2024-06-11 21:13:33 +0300

    commit 9

--- 14/14.php
+++ 14/14.php
@@ -1,47 +1,49 @@
 <?php
 /*
+    * [git checkout] = [git status]
+    * [git checkout main]         =>> to switch to [main branch]
+
     =>> git stash:
-    * index [0, 1, 2, 3, 4].
+    * index [0, 1, 2, 3, 4]
     * 0  =>> newest stash
     * 4  =>> oldest stash
     
-    =>> git stash pop       =>> [last in]->[first out] = index [0] =>> pop one index only
+    =>> git stash           =>> to execute  =>> files in [stage area]       =>> [best case]
+    =>> git stash pop       =>> to execute  =>> no [untracked files]    =>> or commited to [local repo]
+                            =>> [last in]->[first out] = index [0] =>> pop one index only
 
-    * touch my_file.txt
-    * nano my_file.txt
-    * git add readme.txt
-    * git stash             =>> stash [untracked files] in [working directory] -> [stash box] from [git], [windows]
-                            =>> even staged
-                            =>> not through [view hidden]
+    * touch AJAX
+    * nano AJAX
+    * git add AJAX
+    * git stash             =>> stash files in [stage area] -> [stash box] from [git], [windows]
+                            =>> not through [view -> hidden]
 
-    * touch JS
+    * nano JS                           =>> [file content] can't be empty
     * git add JS
-    * git stash save "JS file"          =>> stash file with [text msg].
+    * git stash save "JS file"          =>> stash files with [text msg]
     ** git stash list
     
     * touch TS
     * git add TS
-    * git stash                         =>> stash file in [stash box].
+    * git stash                         =>> stash files in [stash box]
     
-    * git stash pop                     =>> unstash files from [stash box] =>> and deleting it's index.
-    * git stash apply                   =>> unstash files from [stash box] =>> and not deleting it's index.
+    * git stash pop     =>> unstash files from [stash box] -> [stage area] =>> and deleting it's index
+    * git stash apply   =>> unstash files from [stash box] -> [stage area] =>> not deleting it's index
     
-        => repeating this command =>> [to get same files out] of stash box.
-        =>> + if i stashed it, it will stash it in a new stash.
-        =>> and the old stash =>> remain the same.
-        =>> two stashes =>> with the same content.
+    * [git stash apply stash@{1}]
+    * [git stash]                       =>> [new stash index] =>> same content in [old stash index]
 
-    * ls                                =>> to display [files and folders] in my computer.
+    * ls                                =>> to display [files and folders] in this repo
     
-    * git stash pop stash@{1}           =>> to get files out of stash box =>> [specific file] and deleting it's index.
-    * git stash apply stash@{1}         =>> to get files out of stash box =>> [specific file] and not deleting it's                                                                                                                 index. 
+    * git stash pop stash@{1}           =>> unstash file from [stash box] =>> [specific index] and deleting it's index.
+    * git stash apply stash@{1}         =>> unstash file from [stash box] =>> [specific index] not deleting it's index. 
     
-    * git stash drop                    =>> to delete the newest stash with it's content =>> index = 0.
-    * git stash drop stash@{}           =>> to delete specific stash with it's content.
-    * git stash clear                   =>> to delete all stashes with their content.   
+    * git stash drop                    =>> delete [newest stash index] with content
+    * git stash drop stash@{1}          =>> delete [specific stash index] with content
+    * git stash clear                   =>> delete [all stash indexes] with content
     
-    ** git stash show                   =>> to show the content of the newest stash =>> index = 0.
-    ** git stash show stash@{1}         =>> to show the content of specific stash.
+    ** git stash show                   =>> to show the content of the newest stash =>> index = 0
+    ** git stash show stash@{1}         =>> to show the content of specific stash
     
     * H.W [Home Work] =>> make a branch from specific stash?
 */
