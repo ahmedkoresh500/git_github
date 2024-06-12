a5f52ac9da2d5079d8ec24059ef353a8a94cc345
ahmedkoresh500 <ahmedkoresh500@gmail.com>
2024-06-12 21:49:44 +0300

    commit 27

--- 19_last_one/19.php
+++ 19_last_one/19.php
@@ -2,16 +2,16 @@
 /*
     * about tags and releases:
     
-    * [git tag] = [git tag -l]          =>> to list all tags
+    * [git tag]                         =>> to list all tags  =>> two are the same
+    * [git tag -l]                      =>> to list all tags  =>> two are the same
     
     * git tag -l "v1.0"                 =>> to list specific tag
+    * git tag -l "v1.*"                 =>> to list all tags [start with] [v1.]
     
-    * git tag -l "v1.*"                 =>> to list all first versions of a tag
+    * [git tag -d v1.0]                 =>> to delete specific tag [local repo]  =>> two are the same
+    * [git tag --delete "v1.0"]         =>> to delete specific tag [local repo]  =>> two are the same
     
-    * [git tag -d v1.0]                 =>> two are the same
-    * [git tag --delete "v1.0"]         =>> two are the same =>> to delete specific tag locally =>> on gitbash.
-    
-    * [git push origin -d v2.0]         =>> two are the same  =>> to delete specific tag on github.
-    * [git push origin --delete v2.0]   =>> two are the same  =>> to delete specific tag on github.
+    * [git push origin -d v2.0]         =>> to delete specific tag [on github]  =>> two are the same
+    * [git push origin --delete v2.0]   =>> to delete specific tag [on github]  =>> two are the same
 */
 ?>
