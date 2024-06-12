247ce782bba67d85f9cf3069eba33c42ac702657
ahmedkoresh500 <ahmedkoresh500@gmail.com>
2024-06-12 15:58:03 +0300

    commit 15

--- 17/17.php
+++ 17/17.php
@@ -5,26 +5,24 @@
         * [not staged] [not committed]
 
     * note:
-        =>> [*.log]                 =>> to ignore all files [end with] [.log]
-        =>> [!vip.log]              =>> not ignore [this file]
+        =>> [*.log]             =>> to ignore all files [end with] [.log]
+        =>> [!vip.log]          =>> not ignore [this file]
     
-    * touch vip.log                     =>> not ignore [this file]  =>> [!vip.log] -> [.gitignore] file
-    * mkdir node_packs                  =>> ignore [this folder] =>> [node_packs/] -> [.gitignore] file
+    * touch vip.log             =>> not ignore [this file]  =>> [!vip.log] -> [.gitignore] file
+    * mkdir node_packs          =>> ignore [this folder] =>> [node_packs/] -> [.gitignore] file
     * cd node_packs/
     * touch hello.php
     * cd ..
     
-    * [touch .gitignore]                =>> to create new file.    
-    * [code .gitignore]                 =>> open with [vscode editor]
+    * [touch .gitignore]        =>> to create new file.    
+    * [code .gitignore]         =>> open with [vscode editor]
     
-    * git st
+    * [git st]                =>> [untracked files] [working directory]
+    * [ls]                    =>> to list all [files] and [folders] in this directory
     
     =>> we can ignore [.gitignore] file itself      =>> doesn't work  =>> i don't know why
 
-    =>> [git st]                    =>> [untracked files] [working directory]
-    =>> [ls]                        =>> to list all [files] and [folders] in this directory
-
-    * git add -f text.txt           =>> [untracked file][working directory] -> [stage area] although ignored
-    * git add --force text.txt      =>> [untracked file][working directory] -> [stage area] although ignored
+    * git add -f text.txt       =>> [untracked file][working directory] -> [stage area] although ignored
+    * git add --force text.txt  =>> [untracked file][working directory] -> [stage area] although ignored
 */
 ?>
